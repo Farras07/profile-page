634181a0f7696a43737fafd51c523d8cc8b47bff
Farras07 <21082010057@student.upnjatim.ac.id>
2023-10-20 03:24:01 +0700

    finish

---
+++ tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#498af2',
+          dust: '#cde4f7',
+          clearsky:'#73bdfa',
+          sea: '#0f90fa'
+        }
+      }
+    }
+  }
